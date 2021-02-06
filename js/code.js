17cb9f80cdf0fec375d2080e3a3bfb9a8eeda6d0
Camila <camila.larosap@gmail.com>
2021-02-06 15:07:27 -0500

    updated main and styles in general

--- js/code.js
+++ js/code.js
@@ -1,7 +1,8 @@
-var urlBase = 'http://mycontacts.studio/LAMPAPI';
+var urlBase = 'http://www.srslystudy.ninja/LAMPAPI'
 var extension = 'php';
 
-var userId = 0;
+var userId = 0; 
+var contactId = 0;
 var firstName = "";
 var lastName = "";
 
@@ -67,9 +68,10 @@ function doRegister()
 	
 	var login = document.getElementById("loginName").value;
 	var password = document.getElementById("loginPassword").value;
-  var email = document.getElementById("Email").value;
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
+  email = document.getElementById("Email").value;
+  firstName = document.getElementById("firstName").value;
+  lastName = document.getElementById("lastName").value;
   //var hash = md5( password );
 	
 	document.getElementById("registerResult").innerHTML = "";
@@ -77,6 +79,11 @@ function doRegister()
 //	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
 	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '", "firstName" : "' + firstName + '", "lastName" : "' + lastName + '", "email" : "' + email + '"}';
 	var url = urlBase + '/Register.' + extension;
+ 
+ if (confirmPassword != password) {
+   document.getElementById("registerResult").innerHTML = "Passwords do not match, please try again";
+   return;
+ }
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -89,17 +96,15 @@ function doRegister()
 			{
 				var jsonObject = JSON.parse( xhr.responseText );
 		
-				userId = jsonObject.userId;
+				id = jsonObject.id;
 		
-				if( userId < 1 )
+				if( id < 1 )
 				{
-					document.getElementById("registrationResult").innerHTML = jsonObject.message;
+					document.getElementById("registerResult").innerHTML = jsonObject.message;
 					return;
 				}
-		
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
 
+        doLogin();
 				saveCookie();
 	
 				window.location.href = "main.html";
@@ -110,7 +115,7 @@ function doRegister()
 	}
 	catch(err)
 	{
-		document.getElementById("registrationResult").innerHTML = err.message;
+		document.getElementById("registerResult").innerHTML = err.message;
 	}
 
 }
@@ -120,7 +125,7 @@ function saveCookie()
 	var minutes = 20;
 	var date = new Date();
 	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ",contactId=" + contactId;"expires=" + date.toGMTString();
 }
 
 function readCookie()
@@ -143,6 +148,10 @@ function readCookie()
 		else if( tokens[0] == "userId" )
 		{
 			userId = parseInt( tokens[1].trim() );
+		}
+   else if( tokens[0] == "contactId" )
+		{
+			contactId = parseInt( tokens[1].trim() );
 		}
 	}
 	
@@ -174,60 +183,21 @@ function registerDisappear()
   document.getElementById("showRegistration").style.display = "none";
   document.getElementById("showLogin").style.display = "none";
 }
-/*
-function doLogintest()
+
+function addContact()
 {
-	userId = 0;
-	firstName = "";
-	lastName = "";
+  var cfirstName = document.getElementById("firstNameCont").value;
+	var clastName = document.getElementById("lastNameCont").value;
+	var cemail = document.getElementById("emailCont").value;
+	var cphone = document.getElementById("phoneCont").value;
+	var caddress = document.getElementById("addressCont").value;
+	var czipCode = document.getElementById("zipCodeCont").value;
+	var ccity = document.getElementById("cityCont").value;
+	var cstate = document.getElementById("stateCont").value;
+	document.getElementById("contactAddResult").innerHTML = "";
 	
-	var fName = document.getElementById("firstName").value;
-	var lName = document.getElementById("lastName").value;
-	var login = document.getElementById("loginName").value;
-	var password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
-	
-	document.getElementById("loginResult").innerHTML = "";
-
-	var jsonPayload = '{"firstName" : "' + fName + '", "lastName" : "' + lName + '"';
-	  jsonPayload += ', "login" : "' + login + '", "password" : "' + password + '"}';
-	var url = urlBase + '/Register.' + extension;
-	console.log(jsonPayload);
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.send(jsonPayload);
-		
-		var jsonObject = JSON.parse( xhr.responseText );
-		console.log(jsonObject);
-		userId = jsonObject.id;
-		
-		if( userId < 1 )
-		{
-			document.getElementById("loginResult").innerHTML = jsonObject.message;
-			return;
-		}
-		
-
-	
-		window.location.href = "color.html";
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
-
-}
-function addColor()
-{
-	var newColor = document.getElementById("colorText").value;
-	document.getElementById("colorAddResult").innerHTML = "";
-	
-	var jsonPayload = '{"color" : "' + newColor + '", "userId" : ' + userId + '}';
-	var url = urlBase + '/AddColor.' + extension;
+	var jsonPayload = '{"firstName" : "' + cfirstName + '", "lastName" : "' + clastName + '", "email" : "' + cemail + '", "phone" : "' + cphone + '", "address" : "' + caddress + '", "zipCode" : "' + czipCode + '", "city" : "' + ccity + '", "state" : "' + cstate + '", "userId" : ' + userId + '}';
+	var url = urlBase + '/Create.' + extension;
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -238,27 +208,27 @@ function addColor()
 		{
 			if (this.readyState == 4 && this.status == 200) 
 			{
-				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+				document.getElementById("contactAddResult").innerHTML = "Contact has been added";
 			}
 		};
 		xhr.send(jsonPayload);
 	}
 	catch(err)
 	{
-		document.getElementById("colorAddResult").innerHTML = err.message;
+		document.getElementById("contactAddResult").innerHTML = err.message;
 	}
 	
 }
 
-function searchColor()
+function searchContact()
 {
-	var srch = document.getElementById("searchText").value;
-	document.getElementById("colorSearchResult").innerHTML = "";
+	var name = document.getElementById("searchText").value;
+	document.getElementById("contactSearchResult").innerHTML = "";
 	
-	var colorList = "";
+	var contactList = [['firstName', 'lastName', 'contactID']];
 	
-	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
-	var url = urlBase + '/SearchColors.' + extension;
+	var jsonPayload = '{"name" : "' + name + '","userId" : ' + userId + '}';
+	var url = urlBase + '/Search.' + extension;
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -269,27 +239,262 @@ function searchColor()
 		{
 			if (this.readyState == 4 && this.status == 200) 
 			{
-				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
 				var jsonObject = JSON.parse( xhr.responseText );
+        document.getElementById("contactSearchResult").innerHTML = jsonObject.num_rows > 0 ? "Contact(s) has been retrieved" : "Contact wasn't found";
+  
+        //var test = jsonObject.contacts[1].firstName;
 				
-				for( var i=0; i<jsonObject.results.length; i++ )
+			  for( var i=0; i<jsonObject.num_rows; i++ )
 				{
-					colorList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
-						colorList += "<br />\r\n";
-					}
+					contactList.push([jsonObject.contacts[i].firstName, jsonObject.contacts[i].lastName, jsonObject.contacts[i].contactId]);
 				}
-				
-				document.getElementsByTagName("p")[0].innerHTML = colorList;
+        
+        console.table(contactList);
+        document.getElementById("contactTable").innerHTML = array2table(contactList);
 			}
 		};
 		xhr.send(jsonPayload);
 	}
 	catch(err)
 	{
-		document.getElementById("colorSearchResult").innerHTML = err.message;
+		document.getElementById("contactSearchResult").innerHTML = err.message;
 	}
 	
 }
-*/
+
+function viewContact(conId)
+{
+  contactId = conId;
+  saveCookie();
+  window.location.href = "view.html";
+}
+
+function view()
+{
+  var jsonPayload = '{"contactId" : ' + contactId + '}';
+	var url = urlBase + '/Read.' + extension;
+  
+  var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+        var jsonObject = JSON.parse( xhr.responseText );
+				//document.getElementById("firstName").innerHTML = firstName;
+        document.getElementById("firstNameView").innerHTML = "First Name: " + jsonObject.firstName;
+        document.getElementById("lastNameView").innerHTML = "Last Name: " + jsonObject.lastName;
+        document.getElementById("emailView").innerHTML = "Email: " + jsonObject.email;
+        document.getElementById("phoneView").innerHTML = "Phone #: " + jsonObject.phone;
+        document.getElementById("addressView").innerHTML = "Address: " + jsonObject.address;
+        document.getElementById("zipCodeView").innerHTML = "Zip Code: " + jsonObject.zipcode;
+        document.getElementById("cityView").innerHTML = "City: " + jsonObject.city;
+        document.getElementById("stateView").innerHTML = "State: " + jsonObject.state;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		//document.getElementById("contactAddResult").innerHTML = err.message;
+	}
+}
+
+
+function array2table(array)
+{
+  var table = "<table class='infoTable'>";
+  var link = "view.html";
+  for (var i = 1; i < array.length; i++)
+  {
+    table += "<tr>";
+    for (var j = 0; j < 5; j++)
+    {
+      if (j < array[i].length)
+      {
+        if (array[i][j] == "")
+        {
+          table += "<td> N/A </td>";
+        }
+        else
+        {
+          if (j == 0)
+          {
+            table += "<td>" + array[i][j] + "</td>";
+          }
+          else if (j == 2)
+          {
+          
+            //var val = parseInt(array[i][j], 10);
+            table += "<td><button type='button' class='buttons' onclick='viewContact(" + array[i][j] + ")';> View </button></td>";
+            table += "<td><button type='button' class='buttons' onclick='editContact(" + array[i][j] + ")';> Edit </button></td>";
+            table += "<td id='del"+array[i][j]+"'><button type='button' class='buttons' onclick='deleteContact(" + array[i][j] + ")';> Delete </button></td>";
+          }
+          else
+          {
+            table += "<td>" + array[i][j] + "</td>";
+          }
+        }
+      }
+      if (j == 3)
+      {
+      }
+      if (j == 4)
+      {
+      }
+    }
+    table += "</tr>";
+  }
+  table += "</table>";
+  return table;
+}
+
+function editContact(conId)
+{
+  contactId = conId;
+  saveCookie();
+  window.location.href = "edit.html";
+}
+
+function edit()
+{
+  var jsonPayload = '{"contactId" : ' + contactId + '}';
+	var url = urlBase + '/Read.' + extension;
+  
+  var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+        var jsonObject = JSON.parse( xhr.responseText );
+				//document.getElementById("firstName").innerHTML = firstName;
+        document.getElementById("firstName").value = jsonObject.firstName;
+        document.getElementById("lastName").value = jsonObject.lastName;
+        document.getElementById("email").value = jsonObject.email;
+        document.getElementById("phone").value = jsonObject.phone;
+        document.getElementById("address").value = jsonObject.address;
+        document.getElementById("zipCode").value = jsonObject.zipcode;
+        document.getElementById("city").value = jsonObject.city;
+        document.getElementById("state").value = jsonObject.state;
+        
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		//document.getElementById("contactAddResult").innerHTML = err.message;
+	}
+}
+
+function confirmEdit()
+{
+  var first = document.getElementById("firstName").value;
+	var last = document.getElementById("lastName").value;
+	var email = document.getElementById("email").value;
+	var phone = document.getElementById("phone").value;
+	var address = document.getElementById("address").value;
+	var zipCode = document.getElementById("zipCode").value;
+	var city = document.getElementById("city").value;
+	var state = document.getElementById("state").value;
+	document.getElementById("contactEditResult").innerHTML = "";
+	
+	var jsonPayload = '{"firstName" : "' + first + '", "lastName" : "' + last + '", "email" : "' + email + '", "phone" : "' + phone + '", "address" : "' + address + '", "zipCode" : "' + zipCode + '", "city" : "' + city + '", "state" : "' + state + '", "contactId" : ' + contactId + '}';
+	var url = urlBase + '/Update.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactEditResult").innerHTML = "Contact has been updated";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactEditResult").innerHTML = err.message;
+	}
+}
+
+function deleteContact(conId)
+{
+  contactId = conId;
+  saveCookie();
+  confirmDelete();
+}
+
+function deleteC()
+{
+  var jsonPayload = '{"contactId" : ' + contactId + '}';
+	var url = urlBase + '/Delete.' + extension;
+  
+  var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+        var jsonObject = JSON.parse( xhr.responseText );
+        
+        id = jsonObject.id;
+		
+				if( id < 1 )
+				{
+					//document.getElementById("deleteResult").innerHTML = jsonObject.message;
+					return;
+				}
+        else
+        {
+          $("td#del" + contactId).parent().remove();
+        }
+        
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		//document.getElementById("deleteResult").innerHTML = err.message;
+	}
+}
+
+function confirmDelete()
+{
+  if (confirm("Are you sure you want to Delete the Contact?")) 
+  {
+    deleteC();
+  } 
+  else 
+  {
+    return; 
+  }
+}
+
+$(document).ready(function(){
+  $(".collapsible").click(function(){
+this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+
+});
